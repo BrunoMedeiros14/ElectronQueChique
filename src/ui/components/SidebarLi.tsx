681a867c692ff9e8f59310ffab14f86be3bbdd87
Bruno Medeiros <brunodemedeiros14@gmail.com>
2024-04-25 23:05:53 -0300

    Separar páginas e componentes da rota.

--- src/ui/components/SidebarLi.tsx
+++ src/ui/components/SidebarLi.tsx
@@ -1,22 +1,23 @@
-import {Link} from "@tanstack/react-router";
-import {ReactNode} from "react";
+import { Link } from "@tanstack/react-router";
+import { ReactNode } from "react";
 
 type SidebarLiProps = {
   texto: string;
   rota: string;
   icone: ReactNode;
+  ativo?: boolean;
 };
 
-export const SidebarLi = ({texto, rota, icone}: SidebarLiProps) => {
+export const SidebarLi = ({ texto, rota, icone, ativo }: SidebarLiProps) => {
   return (
-      <li>
-        <Link
-            to={rota}
-            className="flex items-center p-2 text-gray-900 rounded-lg group [&.active]:bg-gray-200 [&.active]:bg-opacity-75 hover:bg-gray-200"
-        >
-          {icone}
-          <span className="ms-3">{texto}</span>
-        </Link>
-      </li>
+    <li>
+      <Link
+        to={rota}
+        className="flex items-center p-2 text-gray-900 rounded-lg group [&.active]:bg-gray-200 [&.active]:bg-opacity-75 hover:bg-gray-200"
+      >
+        {icone}
+        {ativo && <span className="ms-3">{texto}</span>}
+      </Link>
+    </li>
   );
 };
--- src/ui/routes.tsx
+++ src/ui/routes.tsx
@@ -1,69 +1,36 @@
 import {
   createRootRoute,
   createRoute,
-  createRouter,
-  Outlet,
+  createRouter
 } from "@tanstack/react-router";
-import { Package2, ShoppingBag, Users, Wallet } from "lucide-react";
-import { SidebarLi } from "./components/SidebarLi";
+import { Sidebar } from "./components/Sidebar";
+import { Caixa } from "./pages/Caixa";
 import { Clientes } from "./pages/Clientes";
 import { Contas } from "./pages/Contas";
+import { Estoque } from "./pages/Estoque";
 
 const rootRoute = createRootRoute({
-  component: () => (
-    <>
-      <aside
-        id="sidebar-multi-level-sidebar"
-        className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
-        aria-label="Sidebar"
-      >
-        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-100">
-          <ul className="space-y-2 font-medium">
-            <SidebarLi icone={<ShoppingBag />} texto="Caixa" rota="/caixa" />
-            <SidebarLi icone={<Package2 />} texto="Estoque" rota="/estoque" />
-            <SidebarLi icone={<Users />} texto="Clientes" rota="/clientes" />
-            <SidebarLi icone={<Wallet />} texto="Contas" rota="/contas" />
-          </ul>
-        </div>
-      </aside>
-
-      <div className="p-4 sm:ml-64">
-        <Outlet />
-      </div>
-    </>
-  ),
+  component: () => <Sidebar />,
 });
 
 const indexRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/",
   component: function Index() {
-    return <h1>Home page</h1>;
+    return <h1>Tela de boas vindas!</h1>;
   },
 });
 
 const caixaRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/caixa",
-  component: function About() {
-    return (
-      <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg">
-        caixa
-      </div>
-    );
-  },
+  component: () => <Caixa />,
 });
 
 const estoqueRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/estoque",
-  component: function About() {
-    return (
-      <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg">
-        estoque
-      </div>
-    );
-  },
+  component: () => <Estoque />,
 });
 
 const clientesRoute = createRoute({
