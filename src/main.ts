398f780431b5781358ab588a9a722e70b49ac314
Bruno Medeiros <brunodemedeiros14@gmail.com>
2024-04-26 18:39:20 -0300

    Fixar comunicação entre front e back.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { app, BrowserWindow, ipcMain } from 'electron';
+import { app, BrowserWindow } from 'electron';
 import path from 'path';
-import { buscarClientePorId, buscarTodosClientes, criarCliente, editarCliente, removerCliente } from './repository/RepositorioCliente';
-import { BuscarClientePorId, BuscarTodosClientes, CriarCliente, EditarCliente, RemoverCliente } from './shared/Api';
+import { connection } from './config/BancoDeDados';
+import { serviceCliente } from './service/ServiceCliente';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -12,10 +12,11 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 1280,
     height: 720,
-    autoHideMenuBar: true, // ('default' | 'hidden' | 'hiddenInset' | 'customButtonsOnHover');
+    autoHideMenuBar: true,
     title: 'Que chique Gerenciador de estoque',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
     },
   });
 
@@ -30,21 +31,11 @@ const createWindow = () => {
 };
 
 app.whenReady().then(() => {
-
-
-  ipcMain.on('ping', () => console.log('pong'))
-
-  ipcMain.handle("criarCliente", (_, ...args: Parameters<CriarCliente>) => criarCliente(...args))
-  ipcMain.handle("removerCliente", (_, ...args: Parameters<RemoverCliente>) => removerCliente(...args))
-  ipcMain.handle("editarCliente", (_, ...args: Parameters<EditarCliente>) => editarCliente(...args))
-  ipcMain.handle("buscarClientePorId", (_, ...args: Parameters<BuscarClientePorId>) => buscarClientePorId(...args))
-  ipcMain.handle("buscarTodosClientes", (_, ...args: Parameters<BuscarTodosClientes>) => buscarTodosClientes(...args))
+  connection.sync()
   createWindow();
-
 });
 
-// criarCliente({dataNascimento: new Date(), email: "aa", endereco: "a", nome: "a", telefone: "a"})
-// app.on('ready', createWindow);
+app.on('ready', serviceCliente);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -57,3 +48,4 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
--- src/preload.ts
+++ src/preload.ts
@@ -10,14 +10,15 @@ export const apiCliente = {
   removerCliente: (...args: Parameters<RemoverCliente>): ReturnType<RemoverCliente> => ipcRenderer.invoke('removerCliente', ...args),
   editarCliente: (...args: Parameters<EditarCliente>): ReturnType<EditarCliente> => ipcRenderer.invoke('editarCliente', ...args),
   buscarClientePorId: (...args: Parameters<BuscarClientePorId>): ReturnType<BuscarClientePorId> => ipcRenderer.invoke('buscarClientePorId', ...args),
-  buscarTodosClientes: (...args: Parameters<BuscarTodosClientes>): ReturnType<BuscarTodosClientes> => {console.log(2); return ipcRenderer.invoke('buscarTodosClientes', ...args)}
+  buscarTodosClientes: (...args: Parameters<BuscarTodosClientes>): ReturnType<BuscarTodosClientes> => ipcRenderer.invoke('buscarTodosClientes', ...args)
 }
 
 try {
+  contextBridge.exposeInMainWorld('apiCliente', apiCliente)
   contextBridge.exposeInMainWorld('context', {
     locale: navigator.language,
   })
-  contextBridge.exposeInMainWorld('cliente', apiCliente)
+
 } catch (error) {
   console.error(error)
 }
